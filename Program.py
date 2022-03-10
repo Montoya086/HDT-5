9e88f5d13b1296bfa1f6ca5a9df1960a4f9e9964
Montoya086 <mon21552@uvg.edu.gt>
2022-03-09 18:59:35 -0600

    clase memoria

--- Program.py
+++ Program.py
@@ -1,12 +1,24 @@
 import random
 import simpy
 
-class Sistem:
-    def __init__(self, env):
-        self.cpu = simpy.Resource(env, number=1)
-        self.ram = simpy.Container(env, init=100, capacity=100)
-        self.mon_proc = env.process(self.monitor_tank(env))
+PROCESS_NUMBER = 25
+RANDOM_SEED = 30
+CPU_CORES = 1
+INTERVAL = 10
+CAPACITY = 100
+class Proceso:
+    def new():
+    def ready():
+    def waiting():
+    def running():
+    def terminated():
+class memory:
+    def __init__(self, env, cpu_cores, cap):
+        self.cpu = simpy.Resource(env, capacity=cpu_cores)
+        self.ram = simpy.Container(env, init=cap, capacity =cap)
 
 
-
+env = simpy.Environment()
+sis = Sistema(env)
+env.run()
 
